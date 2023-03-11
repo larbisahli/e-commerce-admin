ef01a253406129147c764fd5ac0ba63bbe9f86f9
larbisahli <larbisahli1905@gmail.com>
2023-03-11 03:13:47 +0100

    fix production env

--- utils/utils.ts
+++ utils/utils.ts
@@ -59,11 +59,11 @@ export const replace = (array, index: number, replacerIndex: number) => {
   }
 };
 
-export const mediaURL = !PRODUCTION_ENV
+export const mediaURL = PRODUCTION_ENV
   ? process.env.MEDIA_URL
   : 'http://127.0.0.1:5000/media';
 
-export const apiURL = !PRODUCTION_ENV
+export const apiURL = PRODUCTION_ENV
   ? process.env.API_URL
   : 'http://127.0.0.1:5000';
 
