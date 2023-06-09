f20ed1f8ac71585b43650968651a5977b9701c49
larbisahli <larbisahli1905@gmail.com>
2023-06-09 13:56:41 +0100

    adding ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ import apolloClient from '@lib/apollo-client';
 import store from '@store/index';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 import { appWithTranslation } from 'next-i18next';
 import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
@@ -27,6 +28,19 @@ function App({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop;
   return (
     <Fragment>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GTAG_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
